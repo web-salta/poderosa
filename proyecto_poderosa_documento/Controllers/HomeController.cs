4ad59e1015bd8b6a211d0c8cca30012dfc3871cd
web-salta <web@saltamontes.online>
2025-03-12 01:49:23 -0500

    Segundo commit

---
+++ proyecto_poderosa_documento/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+namespace proyecto_poderosa_documento.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult proveedores()
+        {
+            return View();
+        }
+        public ActionResult contacto()
+        {
+            return View();
+        }
+    }
+}
---
+++ proyecto_poderosa_documento/Controllers/operacionesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+namespace proyecto_poderosa_documento.Controllers
+{
+    public class operacionesController : Controller
+    {
+        // GET: Home
+        public ActionResult introduccion()
+        {
+            return View();
+        }
+
+    }
+}
