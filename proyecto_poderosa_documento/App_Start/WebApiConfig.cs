5f0d3face098379da04b39e5032ca1b2950b353d
web-salta <web@saltamontes.online>
2025-04-28 08:20:01 -0500

    Initial commit del proyecto Poderosa 2025

---
+++ proyecto_poderosa_documento/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace proyecto_poderosa_documento.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+            );
+        }
+    }
+}
