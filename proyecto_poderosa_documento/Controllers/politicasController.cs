9317b13c8040ee6a09c121061bcaa970501a2ca2
web-salta <web@saltamontes.online>
2025-05-22 11:59:01 -0500

    Implementación de politicas de cookies

---
+++ proyecto_poderosa_documento/Controllers/politicasController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace proyecto_poderosa_documento.Controllers
+{
+    public class politicasController : Controller
+    {
+        // GET: politicas
+        public ActionResult politica_cookies()
+        {
+            return View();
+        }
+    }
+}
--- proyecto_poderosa_documento/Global.asax.cs
+++ proyecto_poderosa_documento/Global.asax.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +14,22 @@ namespace proyecto_poderosa_documento
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // Habilitar enrutamiento por atributos
+            System.Web.Routing.RouteTable.Routes.MapMvcAttributeRoutes();
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+
+            int error = httpException != null ? httpException.GetHttpCode() : 0;
+
+            Server.ClearError();
+            // Sanitize the exception message to remove newline characters
+            string sanitizedMessage = exception.Message.Replace("\r", "").Replace("\n", " ");
+            Response.Redirect(String.Format("~/Error/?error={0}&mensaje={1}", error, sanitizedMessage));
         }
     }
 }
